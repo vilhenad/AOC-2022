09c81395f28c029305a870c3e24c8f65d2ef764f
vilhenad <vilhenadp@gmail.com>
2022-12-06 16:34:05 +0000

    commit

--- day6.py
+++
@@ -1,4 +0,0 @@
-text = open("d6.txt").read().strip()
-m = lambda n: next(filter(lambda i: len(set(text[i:i+n]))==n, range(len(text)-n)))+n # next acts as a break
-print(m(4)) # part 1
-print(m(14)) # part 2
